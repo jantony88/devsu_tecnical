3e3deaba7a355cf88af6d3a90e27cfa26cf29f32
Jesus Coloma <jcantinio24@gmail.com>
2024-07-24 06:01:19 -0500

    prueba de integracion

--- ClientServicer/src/test/java/com/kawsay/clientservicer/ClientsControllerTest.java
+++ ClientServicer/src/test/java/com/kawsay/clientservicer/ClientsControllerTest.java
@@ -1,2 +1,27 @@
-package com.kawsay.clientservicer;public class ClientsControllerTest {
+package com.kawsay.clientservicer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfiguration
+public class ClientsControllerTest {
+    @Autowired
+    private MockMvc test;
+
+    // Test methods for ClientController
+    @Test
+    public void status_ok200() throws Exception {
+        test.perform(MockMvcRequestBuilders.get("/clientes").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andExpect(MockMvcResultMatchers.content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
 }
